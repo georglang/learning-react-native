647a2b10f1676113c07ee7f62e26f17995a598b4
Georg Lang <georglang0788@gmx.net>
2022-05-08 13:05:03 +0200

    change CounterScreen state to reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+// !!! commented out because replaced by useReducer
+// import React, { useState } from 'react';
 // useState is a Hook
 // Hook:
 // A hook is a function that adds some new functionality to a function component
 
+import React, { useReducer } from 'react';
+
 import { View, StyleSheet, Text, Button } from 'react-native';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
 // function component
 const CounterScreen = () => {
@@ -25,22 +39,27 @@ const CounterScreen = () => {
   // is executed again
   // but the second time it reruns the value of counter is updated
 
+  // !!! commented out because replaced by useReducer
+  // const [counter, setCounter] = useState(0);
 
-  const [counter, setCounter] = useState(0);
+  const initStateObject = { count: 0 };
+
+  const [state, dispatch] = useReducer(reducer, initStateObject);
+  const { count } = state;
 
   return (
     <View>
-      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-      <Text style={styles.counterStyle}>{counter}</Text>
+      <Button title="Increase" onPress={() => dispatch({ type: 'increment', payload: 10 })} />
+      <Button title="Decrease" onPress={() => dispatch({ type: 'decrement', payload: 1 })} />
+      <Text style={styles.counterStyle}>{count}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   counterStyle: {
-    fontSize: 40
-  }
+    fontSize: 40,
+  },
 });
 
 export default CounterScreen;
